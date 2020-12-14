ce4058eb8e66fe625e5414ad82a3693d0bb2bf25
mbone1 <m.bonnev10@gmail.com>
2020-12-14 12:00:18 -0700

    task displaying

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import '../src/stylesheets/buttons.scss'
 import React, { useState } from 'react';
 import playlists from './playlists.js';
 
+
+
+function Task({ task }) {
+  return (
+    <div className="goal">
+      {task.text}
+    </div>
+  )
+} 
+
 function App() {
   
   const [source, setSource] = useState("") //variable created to swap 'src' and change video displayed
+  const [value, setValue] = useState("")
+  const [tasks, setTasks] = useState([
+    { text: "Rework Front-End"},
+  ])
   
   
   
@@ -31,25 +45,29 @@ function App() {
     <div className="App">
       <div class="columns">
         <div class="column is-one-fifth has-text-right">
-         
           <div class="list scroller has-text-right">{listItems}</div>
         </div>
 
-        <div class="column is-four-fifths">
-          <div class="columns"></div>
-          <div class="row">&nbsp;</div>
-          <div class="has-text-centered">
-            <p class="woo">45:00 </p>
+        <div class="column is-three-fifths">
+          <div class="has-text-left">
+            <div class="timer"></div>
           </div>
           <div class="row">&nbsp;</div>
           <iframe
             class="video"
+            title={source}
             width="840"
             height="472.5"
             src={source}
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen></iframe>
+        </div>
+        <div class="column is-one-fifth">
+          Todays goal is{" "}
+          {tasks.map((task, index) => (
+            <Task key={index} index={index} task={task} >{task}</Task>
+          ))}
         </div>
       </div>
     </div>
