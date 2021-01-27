c5abc1cb1a0f4a77c51ba1c5a01ba076caf82728
mbone1 <m.bonnev10@gmail.com>
2021-01-27 15:58:13 -0700

    scheduler layout complete

--- src/components/autoscheduler/scheduler.js
+++ src/components/autoscheduler/scheduler.js
@@ -23,39 +23,45 @@ const [date, setDate] = useState();
     return (
       <div className="timecontainer">
         <div className="borders timeBlock title invertConst">
-          AUTO SCHEDULER
+          AUTO-SCHEDULER
         </div>
-        <div className="borders timeBlock selectorBox">
-          <span className="selectorText"> Select your shift</span>
-          <br></br>
-          <div className="button-holder">
-            <button className="loc-button invert borders">siesta</button>
-            <button className="loc-button invert borders">straight 8</button>
-            <button className="loc-button invert borders">straight 6</button>
-            <button className="loc-button invert borders">straight 4</button>
-            <button className="loc-button invert borders">regular 8</button>
-            <button className="loc-button invert borders">regular 6</button>
-            <button className="loc-button invert borders">regular 4</button>
-          </div>
-        </div>
-
         <div className="borders timeBlock">
           <span>custom shift</span>
           <br></br>
           <button className="shift-button invert borders">create</button>
+          <br></br>
         </div>
-        <br></br>
-        <div>ability to enter shifts here (optional hours/minutes)</div>
-        <br></br>
-        <div>ability to enter lunch goes here (optional hours/minutes)</div>
-        <br></br>
-        <div>down here will be relevent info for entire day</div>
-        <br></br>
-        <div>Shift Length will go here</div>
-        <br></br>
-        <div>Start time = {startTime}</div>
-        <div>End time = {endTime}</div>
-        <br></br>
+        <div className="borders timeBlock selectorBox">
+          <span className="selectorText"> premade shifts</span>
+          <br></br>
+          <div className="button-holder">
+            <button className="shift-button invert borders">siesta</button>
+            <button className="shift-button invert borders">straight 8</button>
+            <button className="shift-button invert borders">straight 6</button>
+            <button className="shift-button invert borders">straight 4</button>
+            <button className="shift-button invert borders">regular 8</button>
+            <button className="shift-button invert borders">regular 6</button>
+            <button className="shift-button invert borders">regular 4</button>
+          </div>
+        </div>
+        <div className="borders timeBlock">
+          <span>select tasks</span>
+         
+        </div>
+        <div className="borders timeBlock">
+          <span className="options">Current Shift Start : {startTime}</span>
+          <br></br>
+          <span className="options">Lunch : beginning to end </span>
+          <br></br>
+          <span className="options">Current Shift End : {endTime}</span>
+          <br></br>
+            <button className="shift-button invert borders">begin</button>
+        </div>
+          <br></br>
+          <br></br>
+          <br></br>
+
+     
         <div className="borders timeBlock">
           each block of time will be stored within a bordered div like this
         </div>
