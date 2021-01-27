c78d9501bb96c7b2abbf089dd413bfa330046559
mbone1 <m.bonnev10@gmail.com>
2021-01-26 17:45:50 -0700

    start and end times configured

--- src/components/autoscheduler/scheduler.js
+++ src/components/autoscheduler/scheduler.js
@@ -1,11 +1,14 @@
-import { DateTime } from "luxon";
+import { DateTime, Duration } from "luxon";
 import React, { useState, useEffect } from "react";
 
 
 export default function AutoScheduler(length, tasks, breakLength, breakAmount) {
 const [date, setDate] = useState();
 
-    let startTime = DateTime.local().toLocaleString(DateTime.TIME_WITH_SECONDS)
+    let startTime = DateTime.local().plus({ minutes: 5 }).toLocaleString(DateTime.TIME_WITH_SECONDS)
+    let endTime = DateTime.local().plus({ hours: 6 }).toLocaleString(DateTime.TIME_WITH_SECONDS)
+    let dur = Duration.fromObject({ hours: 6 }) //length will be stored here
+    console.log(dur.as('minutes'))
 
     useEffect(() => {
       let timerID = setInterval(() => tick(), 1000);
@@ -16,9 +19,11 @@ const [date, setDate] = useState();
     function tick() {
       setDate(new Date());
     }
+    
     return (
         <>
             <div>Start time = {startTime}</div>
+            <div>End time = {endTime}</div>
             </>
 
     )
