20d076d9a6847dff0000590bab0fcf437118987d
mbone1 <m.bonnev10@gmail.com>
2021-02-09 12:52:19 -0700

    separated component for adding video

--- src/components/playlist/adder.js
+++ src/components/playlist/adder.js
@@ -1,29 +1,29 @@
 import React, { useState } from "react";
 import { useSourceStore } from "./source.js";
 import '../../stylesheets/adder.scss'
+import SaveVideo from './saveVideo.js'
 
 export default function Adder() {
-  const [video, setVideo] = useState({})
   const [form, setForm] = useState("Watch video")
   const [source, setSource] = useSourceStore()
   
-  const handleChange = (e) => {
-    //changes the value of video object to values entered in fields
-    const { name, value } = e.target;
-    setVideo({
-      ...video,
-      [name]: value,
-    });
-  }
-  const handleSubmit = (e) => {
-    //first prevents reload of page
-    e.preventDefault();
-    if (!video) return;
-    saveToLocalStorage(video)
-    setVideo({
-      Name: "",
-      SRC: ""})
-  }
+  // const handleChange = (e) => {
+  //   //changes the value of video object to values entered in fields
+  //   const { name, value } = e.target;
+  //   setVideo({
+  //     ...video,
+  //     [name]: value,
+  //   });
+  // }
+  // const handleSubmit = (e) => {
+  //   //first prevents reload of page
+  //   e.preventDefault();
+  //   if (!video) return;
+  //   saveToLocalStorage(video)
+  //   setVideo({
+  //     Name: "",
+  //     SRC: ""})
+  // }
 
   const handleWatchVid = (e) => {
     e.preventDefault();
@@ -31,49 +31,18 @@ export default function Adder() {
     setSource(e.target[0].value);
   }
 
-  //function below saves to storage
-  const saveToLocalStorage = (data) => {
-    //creates empty array
-    let listArray = [];
-    //takes data from storage, parses out and places into empty array
-    listArray = JSON.parse(localStorage.getItem("playlist")) || [];
-    //pushes new data into playlist
-    listArray.push(data);
-    //re-adds back to local storage with addition of new data
-    localStorage.setItem("playlist", JSON.stringify(listArray));
-  }
+
   return (
     <div className="threeFour borders">
       {/* form to add video to local storage */}
-      <form className={form === "Add video" ? "borders formAdder" : "hidden"}>
-        <label className="">NAME </label>
-        <input
-          type="text"
-          name="Name"
-          className="borders"
-          value={video.name}
-          onChange={handleChange}></input>
-        <br></br>
-        <label>&nbsp;&nbsp;URL &nbsp;&nbsp;</label>
-        <input
-          type="text"
-          name="SRC"
-          className="borders"
-          value={video.value}
-          onChange={handleChange}></input>
-        <br></br>
-        <button
-          className="invert submitbutton borders3"
-          type="submit"
-          onClick={handleSubmit}>
-          {form}
-        </button>
-        <span></span>
-      </form>
+      <div className={form === "Add video" ? "borders" : "hidden"}>
+      <SaveVideo/>
+      </div>
+     
 
       {/* form to source of video, does not save video*/}
       <form
-        type='submit'
+        type="submit"
         className={form === "Watch video" ? "borders formAdder" : "hidden"}
         onSubmit={handleWatchVid}>
         <label>&nbsp;&nbsp;URL &nbsp;&nbsp;</label>
@@ -82,15 +51,11 @@ export default function Adder() {
           name="SRC"
           className="borders"
           // value={source}
-          ></input>
-        <button
-          className="invert submitbutton borders3"
-          type="submit">
+        ></input>
+        <button className="invert submitbutton borders3" type="submit">
           {form}
         </button>
       </form>
-
-
 
       {/* form to add link to local storage */}
 
---
+++ src/components/playlist/saveVideo.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+export default function SaveVideo() {
+const [video, setVideo] = useState({});
+
+
+  const handleChange = (e) => {
+    //changes the value of video object to values entered in fields
+    const { name, value } = e.target;
+    setVideo({
+      ...video,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    //first prevents reload of page
+    e.preventDefault();
+    if (!video) return;
+    saveToLocalStorage(video);
+    setVideo({
+      Name: "",
+      SRC: "",
+    });
+  };
+  //function below saves to storage
+  const saveToLocalStorage = (data) => {
+    //creates empty array
+    let listArray = [];
+    //takes data from storage, parses out and places into empty array
+    listArray = JSON.parse(localStorage.getItem("playlist")) || [];
+    //pushes new data into playlist
+    listArray.push(data);
+    //re-adds back to local storage with addition of new data
+    localStorage.setItem("playlist", JSON.stringify(listArray));
+    };
+
+    return (
+      <form className='formAdder'>
+        <label className="">NAME </label>
+        <input
+          type="text"
+          name="Name"
+          className="borders"
+          value={video.name}
+          onChange={handleChange}></input>
+        <br></br>
+        <label>&nbsp;&nbsp;URL &nbsp;&nbsp;</label>
+        <input
+          type="text"
+          name="SRC"
+          className="borders"
+          value={video.value}
+          onChange={handleChange}></input>
+        <br></br>
+        <button
+          className="invert submitbutton borders3"
+          type="submit"
+          onClick={handleSubmit}>Add Video</button>
+        <span></span>
+      </form>
+    );
+         
+
+}
